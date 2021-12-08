74531deee2a87dcbe8cd5a5a248fa4ed55eca01e
antoine.laurent <antoine.laurent@reseau.eseo.fr>
2021-12-08 10:54:02 +0100

    Added django admin and models

--- fake_news_detector/detector/models.py
+++ fake_news_detector/detector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    content = models.TextField()
+    is_fake_news = models.BooleanField(null=True)
+
