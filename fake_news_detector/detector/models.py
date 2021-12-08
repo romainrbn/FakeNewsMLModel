41e7e1a6aa0c02d2ba6e88b65fc2aead61d17e30
antoine.laurent <antoine.laurent@reseau.eseo.fr>
2021-12-08 16:29:10 +0100

    Added to_dict serializer to Article & a view for all articles (json)

--- fake_news_detector/detector/models.py
+++ fake_news_detector/detector/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
-    content = models.TextField()
     subject = models.CharField(max_length=200)
+    content = models.TextField()
     is_real_news = models.BooleanField(null=True)
 
     def __str__(self):
         return f"Article : {self.title} -- {self.date} -- {self.subject} -- real news : {self.is_real_news}"
+
+    def to_dict(self):
+        dictionary = dict()
+        dictionary['title'] = self.title
+        dictionary['date'] = self.date
+        dictionary['subject'] = self.subject
+        dictionary['content'] = self.content
+        dictionary['is_real_news'] = self.is_real_news
+        return dictionary
--- fake_news_detector/detector/views.py
+++ fake_news_detector/detector/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
-
+from django.http import JsonResponse
+from .models import Article
 
 def index(request):
     return HttpResponse("Hello, world. You're at the detector index.")
+
+def all(request):
+    articles = Article.objects.all()
+    data = [article.to_dict() for article in articles]
+    return JsonResponse(data, safe=False)
 
 def result(request):
     return HttpResponse("This is the results page. Here, the website will tell you if the article you submitted is a fake news or not, with a certainty rate.")
